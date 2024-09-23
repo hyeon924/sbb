8fcfaf31fb3b39e4a180068d9c5d4bde72ab88d1
hyeon924 <hyeonn4713@gmail.com>
2024-09-23 09:36:35 +0900

    1-2, 1-3, 1-4 create

--- src/main/java/com/example/sbb/article/ArticleController.java
+++ src/main/java/com/example/sbb/article/ArticleController.java
@@ -1,12 +1,35 @@
 package com.example.sbb.article;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class ArticleController {
+
+    private final ArticleService articleService;
+
     @GetMapping("/article/list")
-    public String list() {
+    public String list(Model model) {
+        List<Article> articleList = this.articleService.getList();
+        model.addAttribute("articleList", articleList);
         return "article_list";
+    }
+
+    @GetMapping("/article/create")
+    public String articleCreate() {
+        return "article_form";
+    }
+
+    @PostMapping("/article/create")
+    public String articleCreate(@RequestParam(value = "title") String title, @RequestParam(value = "content") String content) {
+        this.articleService.create(title, content);
+        return "redirect:/article/list";
     }
 }
---
+++ src/main/java/com/example/sbb/article/ArticleService.java
@@ -0,0 +1,26 @@
+package com.example.sbb.article;
+
+import lombok.RequiredArgsConstructor;
+import org.checkerframework.checker.units.qual.A;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class ArticleService {
+    private final ArticleRepository articleRepository;
+
+    public List<Article> getList() {
+        return this.articleRepository.findAll();
+    }
+
+    public void create(String title, String content) {
+        Article a = new Article();
+        a.setTitle(title);
+        a.setContent(content);
+        a.setCreateDate(LocalDateTime.now());
+        this.articleRepository.save(a);
+    }
+}
